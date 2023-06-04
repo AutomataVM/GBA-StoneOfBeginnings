2234d33431aa423d503cf329c291000b2022918c
juan pablo fernandez <jpfernandez@cat-technologies.com>
2023-06-04 15:31:04 -0300

    added unsorted files

--- build.bat
+++ build.bat
@@ -104,6 +104,108 @@ script_inserter\swordcraft3c swordcraft3-test.gba script\17d4b7c.txt --pos=17d4b
 script_inserter\swordcraft3c swordcraft3-test.gba script\17d4fac.txt --pos=17d4fac 
 script_inserter\swordcraft3c swordcraft3-test.gba script\17d535c.txt --pos=17d535c 
 
+::sidequests
+script_inserter\swordcraft3c swordcraft3-test.gba script\18c69fc.txt --pos=18c69fc
+script_inserter\swordcraft3c swordcraft3-test.gba script\18c754c.txt --pos=18c754c 
+
+:: restart battle
+script_inserter\swordcraft3c swordcraft3-test.gba script\1723f2c.txt --pos=1723f2c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724a1c.txt --pos=1724a1c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724b3c.txt --pos=1724b3c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724c5c.txt --pos=1724c5c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724d7c.txt --pos=1724d7c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724e9c.txt --pos=1724e9c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1724fbc.txt --pos=1724fbc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725acc.txt --pos=1725acc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725bec.txt --pos=1725bec.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725d0c.txt --pos=1725d0c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725e3c.txt --pos=1725e3c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725f4c.txt --pos=1725f4c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1725f4c.txt --pos=1725f4c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1726a6c.txt --pos=1726a6c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1726b8c.txt --pos=1726b8c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1726cac.txt --pos=1726cac.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1726e0c.txt --pos=1726e0c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1726f6c.txt --pos=1726f6c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1727a2c.txt --pos=1727a2c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1727b5c.txt --pos=1727b5c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1727c7c.txt --pos=1727c7c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1727dac.txt --pos=1727dac.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17246bc.txt --pos=17246bc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17247dc.txt --pos=17247dc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17248fc.txt --pos=17248fc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17252bc.txt --pos=17252bc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17253dc.txt --pos=17253dc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17256cc.txt --pos=17256cc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17257ec.txt --pos=17257ec.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17259ac.txt --pos=17259ac.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17264bc.txt --pos=17264bc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17265ec.txt --pos=17265ec.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17270bc.txt --pos=17270bc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17271dc.txt --pos=17271dc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172404c.txt --pos=172404c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172423c.txt --pos=172423c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172436c.txt --pos=172436c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172447c.txt --pos=172447c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172459c.txt --pos=172459c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172518c.txt --pos=172518c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172559c.txt --pos=172559c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172615c.txt --pos=172615c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172627c.txt --pos=172627c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172639c.txt --pos=172639c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172670c.txt --pos=172670c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172682c.txt --pos=172682c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172694c.txt --pos=172694c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172732c.txt --pos=172732c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172744c.txt --pos=172744c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172756c.txt --pos=172756c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172768c.txt --pos=172768c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172768c.txt --pos=172768c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172f99c.txt --pos=172f99c.txt
+
+
+
+::unsorted
+script_inserter\swordcraft3c swordcraft3-test.gba script\1720dbc.txt --pos=1720dbc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1721b4c.txt --pos=1721b4c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172202c.txt --pos=172202c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\172228c.txt --pos=172228c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\173671c.txt --pos=173671c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\173ccbc.txt --pos=173ccbc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\173d4cc.txt --pos=173d4cc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17430ec.txt --pos=17430ec.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\174348c.txt --pos=174348c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17437cc.txt --pos=17437cc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\174996c.txt --pos=174996c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\174ea3c.txt --pos=174ea3c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\174ee1c.txt --pos=174ee1c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\174f19c.txt --pos=174f19c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175122c.txt --pos=175122c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175779c.txt --pos=175779c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1757bfc.txt --pos=1757bfc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175b16c.txt --pos=175b16c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175b81c.txt --pos=175b81c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175c92c.txt --pos=175c92c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175cf2c.txt --pos=175cf2c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\175f1bc.txt --pos=175f1bc.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\176157c.txt --pos=176157c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\176186c.txt --pos=176186c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1761b5c.txt --pos=1761b5c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\17627ec.txt --pos=17627ec.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1762f3c.txt --pos=1762f3c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\1766d9c.txt --pos=1766d9c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\176713c.txt --pos=176713c.txt
+script_inserter\swordcraft3c swordcraft3-test.gba script\176747c.txt --pos=176747c.txt
+::1748c3c.txt
+::174b7cc.txt
+::17572ec.txt
+::HP recuperation strings
+
+
+
+
+
+
 
 tools\flips --create --ips swordcraft3.gba swordcraft3-test.gba patches\swordcraft3.ips
 tools\flips --create --bps-linear swordcraft3.gba swordcraft3-test.gba patches\swordcraft3.bps
